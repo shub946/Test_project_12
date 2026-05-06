48641441f8e54ae43e0ad590dc406ce00dda0e1e
shub946 <shub5873@gmail.com>
2026-05-06 17:02:05 +0530

    commit 2nd file

---
+++ Test_12/src/test/java/com/test/Sample2.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Sample2 {
+	 @Test
+	    public void testOne() {
+	        System.out.println("Secound test Pass");
+	    }
+	}
